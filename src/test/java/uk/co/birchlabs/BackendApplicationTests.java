542fe23d3cc552c3e9604e3c5a225385520ae2f7
Jamie Birch <jbizzle5@gmail.com>
2016-06-29 16:43:07 +0100

    Remaining tokensToSearch filtered by POS

--- src/test/java/uk/co/birchlabs/BackendApplicationTests.java
+++ src/test/java/uk/co/birchlabs/BackendApplicationTests.java
@@ -1,6 +1,7 @@
 package uk.co.birchlabs;
 
 import catRecurserPkg.*;
+import com.google.common.base.Predicate;
 import com.google.common.collect.Lists;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,6 +17,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
+import static uk.co.birchlabs.JMDictPronunciationRepository2.Mode.READINGS_IN_HIRAGANA;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -77,9 +79,6 @@ public class BackendApplicationTests {
 
 		Set<ForwardingToken> tokensToSearch = new HashSet<>();
 		sortedByFreq.forEach(vocablistRow -> tokensToSearch.add(vocablistRow.getToken()));
-//		Iterable<Integer> ids = Lists.newArrayList(jmDictWordRepository2.getIds(tokensToSearch));
-//		List<JMDictEntry> idWordPairs = Lists.newArrayList(jmDictEntryRepository.findAll(ids));
-        // All entries with a baseForm() found in jmdict_word.
 		List<JMDictEntry> wordEntries = Lists.newArrayList(jmDictEntryRepository2.getEntries(tokensToSearch));
         List<String> baseFormsFound = wordEntries
                 .stream()
@@ -96,6 +95,41 @@ public class BackendApplicationTests {
                 .collect(Collectors.toList()
                 );
         tokensToSearch.removeIf(token -> baseFormsFound.contains(token.getBaseForm()));
+
+        Predicate<ForwardingToken>
+                isNoun = t -> t.getAllFeaturesArray()[0].startsWith("名詞"),
+                isPrefix = t -> t.getAllFeaturesArray()[0].startsWith("接頭詞"),
+                isAdj = t -> t.getAllFeaturesArray()[0].startsWith("形容詞"),
+                isAdverb = t -> t.getAllFeaturesArray()[0].startsWith("副詞"),
+                isAdnominal = t -> t.getAllFeaturesArray()[0].startsWith("連体詞"),
+                isConjunction = t -> t.getAllFeaturesArray()[0].startsWith("接続詞"),
+                isParticle = t -> t.getAllFeaturesArray()[0].startsWith("助詞"),
+                isExclamation = t -> t.getAllFeaturesArray()[0].startsWith("感動詞"),
+                isSymbol = t -> t.getAllFeaturesArray()[0].startsWith("記号"),
+                isFiller = t -> t.getAllFeaturesArray()[0].startsWith("フィラー"),
+                isOther = t -> t.getAllFeaturesArray()[0].startsWith("その他")
+                        ;
+
+        // includes auxiliaries because we conglomerate いる into one row.
+
+        List<ForwardingToken>
+                verbs = tokensToSearch.stream().filter(ForwardingToken::isVerb).collect(Collectors.toList()),
+                nouns = tokensToSearch.stream().filter(isNoun::apply).collect(Collectors.toList()),
+                prefixes = tokensToSearch.stream().filter(isPrefix::apply).collect(Collectors.toList()),
+                adjectives = tokensToSearch.stream().filter(isAdj::apply).collect(Collectors.toList()),
+                adverbs = tokensToSearch.stream().filter(isAdverb::apply).collect(Collectors.toList()),
+                adnominals = tokensToSearch.stream().filter(isAdnominal::apply).collect(Collectors.toList()),
+                conjunctions = tokensToSearch.stream().filter(isConjunction::apply).collect(Collectors.toList()),
+                particles = tokensToSearch.stream().filter(isParticle::apply).collect(Collectors.toList()),
+                exclamations = tokensToSearch.stream().filter(isExclamation::apply).collect(Collectors.toList()),
+                symbols = tokensToSearch.stream().filter(isSymbol::apply).collect(Collectors.toList()),
+                fillers = tokensToSearch.stream().filter(isFiller::apply).collect(Collectors.toList()),
+                others = tokensToSearch.stream().filter(isOther::apply).collect(Collectors.toList());
+
+
+//        List<JMDictEntry> pronunEntries = Lists.newArrayList(jmDictEntryRepository2.getEntries(tokensToSearch, READINGS_IN_HIRAGANA));
+//        pronunEntries.forEach(reading -> wordsFound.add(Utils.convertKana(reading.getIdDataKey().getData())));
+//        tokensToSearch.removeIf(token -> wordsFound.contains(token.getReading()));
 		System.out.println("You're too slow!");
 	}
 
