0280e5113508725f310607ad575b79febd2a43e0
Jamie Birch <jbizzle5@gmail.com>
2016-07-03 12:25:08 +0100

    comments removed

--- src/test/java/uk/co/birchlabs/BackendApplicationTests.java
+++ src/test/java/uk/co/birchlabs/BackendApplicationTests.java
@@ -95,143 +95,22 @@ public class BackendApplicationTests {
         tokensToSearch.removeIf(token -> baseFormsFound.contains(token.getBaseForm()));
 
         TokensByMecabPOS tokensByMecabPOS = new TokensByMecabPOS(tokensToSearch);
-//        List<ForwardingToken>
-//                particles = new ArrayList<>(),
-//                verbsAndAux = new ArrayList<>(), // includes auxiliaries because we conglomerate いる into one row.
-//                adverbs = new ArrayList<>(),
-//                conjunctions = new ArrayList<>(),
-//                nouns = new ArrayList<>(),
-//                prefixes = new ArrayList<>(),
-//                adjectives = new ArrayList<>(),
-//                adnominals = new ArrayList<>(),
-//                exclamations = new ArrayList<>(),
-//                symbols = new ArrayList<>(),
-//                fillers = new ArrayList<>(),
-//                others = new ArrayList<>(),
-//                unclassified = new ArrayList<>();
-//
-//		tokensToSearch.forEach(t -> {
-//            String firstFeature = t.getAllFeaturesArray()[0];
-//            if (firstFeature.startsWith("助詞")) particles.add(t);
-//            else if (t.isVerb()) verbsAndAux.add(t);
-//            else if (firstFeature.startsWith("副詞")) adverbs.add(t);
-//            else if (firstFeature.startsWith("接続詞")) conjunctions.add(t);
-//            else if (firstFeature.startsWith("名詞")) nouns.add(t);
-//            else if (firstFeature.startsWith("接頭詞")) prefixes.add(t);
-//            else if (firstFeature.startsWith("形容詞")) adjectives.add(t);
-//            else if (firstFeature.startsWith("連体詞")) adnominals.add(t);
-//            else if (firstFeature.startsWith("感動詞")) exclamations.add(t);
-//            else if (firstFeature.startsWith("フィラー")) fillers.add(t);
-//            else if (firstFeature.startsWith("その他")) others.add(t);
-//            else if (firstFeature.startsWith("記号")) symbols.add(t);
-//            else unclassified.add(t);
-//        });
 
 
         EntriesByMecabPOS entriesByMecabPOSHiragana = new EntriesByMecabPOS(tokensByMecabPOS, Mode.READINGS_IN_HIRAGANA);
+
 		// List of all JMDictEntrys with a valid hiragana reading
-//        List<JMDictEntry>
-//                particlesByPron = Lists.newArrayList(jmDictPronRepo2.getEntriesFromPron(particles, POS.particles)),
-//				// size = 48 for 26 verbsAndAux (mainly due to suru/aru/iru). Costs 5 seconds.
-//                verbsByPron = Lists.newArrayList(jmDictPronRepo2.getEntriesFromPron(verbsAndAux, POS.verbsAndAux)),
-//                adverbsByPron = Lists.newArrayList(jmDictPronRepo2.getEntriesFromPron(adverbs, POS.adverbs)),
-//                conjunctionsByPron = Lists.newArrayList(jmDictPronRepo2.getEntriesFromPron(conjunctions, POS.conjunctions)),
-//				// size = 136 for 151 nouns (due to some being katakana, some being pronouns.
-//                nounsByPron = Lists.newArrayList(jmDictPronRepo2.getEntriesFromPron(nouns, POS.nouns)),
-//                prefixesByPron = Lists.newArrayList(jmDictPronRepo2.getEntriesFromPron(prefixes, POS.prefixes)),
-//                adjectivesByPron = Lists.newArrayList(jmDictPronRepo2.getEntriesFromPron(adjectives, POS.adjectives)),
-//                adnominalsByPron = Lists.newArrayList(jmDictPronRepo2.getEntriesFromPron(adnominals, POS.adnominals)),
-//                exclamationsByPron = Lists.newArrayList(jmDictPronRepo2.getEntriesFromPron(exclamations, POS.exclamations)),
-//                symbolsByPron = Lists.newArrayList(jmDictPronRepo2.getEntriesFromPron(symbols, POS.symbols)),
-//                fillersByPron = Lists.newArrayList(jmDictPronRepo2.getEntriesFromPron(fillers, POS.fillers)),
-//                othersByPron = Lists.newArrayList(jmDictPronRepo2.getEntriesFromPron(others, POS.others)),
-//                unclassifiedByPron = Lists.newArrayList(jmDictPronRepo2.getEntriesFromPron(unclassified, POS.unclassified))
-//        ;
-
         PronsFoundByMecabPOS pronsFoundByMecabPOSHiragana = new PronsFoundByMecabPOS(entriesByMecabPOSHiragana);
-
-//        List<String>
-//                particlesFound = JMDictEntryRepo2.collectWordsOrPronOfEntries(particlesByPron, CollectionMode.pron),
-//                verbsFound = JMDictEntryRepo2.collectWordsOrPronOfEntries(verbsByPron, CollectionMode.pron),
-//                adverbsFound = JMDictEntryRepo2.collectWordsOrPronOfEntries(adverbsByPron, CollectionMode.pron),
-//                conjunctionsFound = JMDictEntryRepo2.collectWordsOrPronOfEntries(conjunctionsByPron, CollectionMode.pron),
-//                nounsFound = JMDictEntryRepo2.collectWordsOrPronOfEntries(nounsByPron, CollectionMode.pron),
-//                prefixesFound = JMDictEntryRepo2.collectWordsOrPronOfEntries(prefixesByPron, CollectionMode.pron),
-//                adjectivesFound = JMDictEntryRepo2.collectWordsOrPronOfEntries(adjectivesByPron, CollectionMode.pron),
-//                adnominalsFound = JMDictEntryRepo2.collectWordsOrPronOfEntries(adnominalsByPron, CollectionMode.pron),
-//                exclamationsFound = JMDictEntryRepo2.collectWordsOrPronOfEntries(exclamationsByPron, CollectionMode.pron),
-//                symbolsFound = JMDictEntryRepo2.collectWordsOrPronOfEntries(symbolsByPron, CollectionMode.pron),
-//                fillersFound = JMDictEntryRepo2.collectWordsOrPronOfEntries(fillersByPron, CollectionMode.pron),
-//                othersFound = JMDictEntryRepo2.collectWordsOrPronOfEntries(othersByPron, CollectionMode.pron),
-//                unclassifiedFound = JMDictEntryRepo2.collectWordsOrPronOfEntries(unclassifiedByPron, CollectionMode.pron)
-//        ;
 
         // tokensToSearch == 220
         TokensByMecabPOS.updateTokensRemainingToBeSearched(tokensByMecabPOS, pronsFoundByMecabPOSHiragana, tokensToSearch, Mode.READINGS_IN_HIRAGANA);
-//        tokensByMecabPOS.getParticles().forEach(token -> { if(pronsFoundByMecabPOSHiragana.getParticlesFound().contains(Utils.convertKana(token.getReading()))) tokensToSearch.remove(token); });
-//        tokensByMecabPOS.getVerbsAndAux().forEach(token -> { if(pronsFoundByMecabPOSHiragana.getVerbsFound().contains(token.getBaseForm())) tokensToSearch.remove(token); });
-//        tokensByMecabPOS.getAdverbs().forEach(token -> { if(pronsFoundByMecabPOSHiragana.getAdverbsFound().contains(Utils.convertKana(token.getReading()))) tokensToSearch.remove(token); });
-//        tokensByMecabPOS.getConjunctions().forEach(token -> { if(pronsFoundByMecabPOSHiragana.getConjunctionsFound().contains(Utils.convertKana(token.getReading()))) tokensToSearch.remove(token); });
-//        tokensByMecabPOS.getNouns().forEach(token -> { if(pronsFoundByMecabPOSHiragana.getNounsFound().contains(Utils.convertKana(token.getReading()))) tokensToSearch.remove(token); });
-//        tokensByMecabPOS.getPrefixes().forEach(token -> { if(pronsFoundByMecabPOSHiragana.getPrefixesFound().contains(Utils.convertKana(token.getReading()))) tokensToSearch.remove(token); });
-//        tokensByMecabPOS.getAdjectives().forEach(token -> { if(pronsFoundByMecabPOSHiragana.getAdjectivesFound().contains(Utils.convertKana(token.getReading()))) tokensToSearch.remove(token); });
-//        tokensByMecabPOS.getAdnominals().forEach(token -> { if(pronsFoundByMecabPOSHiragana.getAdnominalsFound().contains(Utils.convertKana(token.getReading()))) tokensToSearch.remove(token); });
-//        tokensByMecabPOS.getExclamations().forEach(token -> { if(pronsFoundByMecabPOSHiragana.getExclamationsFound().contains(Utils.convertKana(token.getReading()))) tokensToSearch.remove(token); });
-//        tokensByMecabPOS.getSymbols().forEach(token -> { if(pronsFoundByMecabPOSHiragana.getSymbolsFound().contains(Utils.convertKana(token.getReading()))) tokensToSearch.remove(token); });
-//        tokensByMecabPOS.getFillers().forEach(token -> { if(pronsFoundByMecabPOSHiragana.getFillersFound().contains(Utils.convertKana(token.getReading()))) tokensToSearch.remove(token); });
-//        tokensByMecabPOS.getOthers().forEach(token -> { if(pronsFoundByMecabPOSHiragana.getOthersFound().contains(Utils.convertKana(token.getReading()))) tokensToSearch.remove(token); });
-//        tokensByMecabPOS.getUnclassified().forEach(token -> { if(pronsFoundByMecabPOSHiragana.getUnclassifiedFound().contains(Utils.convertKana(token.getReading()))) tokensToSearch.remove(token); });
         // tokensToSearch == 109
 
         EntriesByMecabPOS entriesByMecabPOSKatakana = new EntriesByMecabPOS(tokensByMecabPOS, Mode.READINGS_IN_KATAKANA);
-//        List<JMDictEntry> // needs to be new lists so we can stream through them, treating katakana
-//        particlesByPron2 = Lists.newArrayList(jmDictPronRepo2.getEntriesFromPron(particles, Mode.READINGS_IN_KATAKANA, POS.particles)),
-//        verbsByPron2 = Lists.newArrayList(jmDictPronRepo2.getEntriesFromPron(verbsAndAux, Mode.READINGS_IN_KATAKANA, POS.verbsAndAux)),
-//        adverbsByPron2 = Lists.newArrayList(jmDictPronRepo2.getEntriesFromPron(adverbs, Mode.READINGS_IN_KATAKANA, POS.adverbs)),
-//        conjunctionsByPron2 = Lists.newArrayList(jmDictPronRepo2.getEntriesFromPron(conjunctions, Mode.READINGS_IN_KATAKANA, POS.conjunctions)),
-//        nounsByPron2 = Lists.newArrayList(jmDictPronRepo2.getEntriesFromPron(nouns, Mode.READINGS_IN_KATAKANA, POS.nouns)), // size: 57
-//        prefixesByPron2 = Lists.newArrayList(jmDictPronRepo2.getEntriesFromPron(prefixes, Mode.READINGS_IN_KATAKANA, POS.prefixes)),
-//        adjectivesByPron2 = Lists.newArrayList(jmDictPronRepo2.getEntriesFromPron(adjectives, Mode.READINGS_IN_KATAKANA, POS.adjectives)),
-//        adnominalsByPron2 = Lists.newArrayList(jmDictPronRepo2.getEntriesFromPron(adnominals, Mode.READINGS_IN_KATAKANA, POS.adnominals)),
-//        exclamationsByPron2 = Lists.newArrayList(jmDictPronRepo2.getEntriesFromPron(exclamations, Mode.READINGS_IN_KATAKANA, POS.exclamations)),
-//        symbolsByPron2 = Lists.newArrayList(jmDictPronRepo2.getEntriesFromPron(symbols, Mode.READINGS_IN_KATAKANA, POS.symbols)),
-//        fillersByPron2 = Lists.newArrayList(jmDictPronRepo2.getEntriesFromPron(fillers, Mode.READINGS_IN_KATAKANA, POS.fillers)),
-//        othersByPron2 = Lists.newArrayList(jmDictPronRepo2.getEntriesFromPron(others, Mode.READINGS_IN_KATAKANA, POS.others)),
-//        unclassifiedByPron2 = Lists.newArrayList(jmDictPronRepo2.getEntriesFromPron(unclassified, Mode.READINGS_IN_KATAKANA, POS.unclassified));
 
         PronsFoundByMecabPOS pronsFoundByMecabPOSKatakana = new PronsFoundByMecabPOS(entriesByMecabPOSKatakana);
-        // Error:(169, 37) java: local variables referenced from a lambda expression must be final or effectively final, so rebuilding nounsFound
-//        List<String>
-//        particlesFound2 = JMDictEntryRepo2.collectWordsOrPronOfEntries(particlesByPron, CollectionMode.pron);
-//        verbsFound2 = JMDictEntryRepo2.collectWordsOrPronOfEntries(verbsByPron, CollectionMode.pron);
-//        adverbsFound2 = JMDictEntryRepo2.collectWordsOrPronOfEntries(adverbsByPron, CollectionMode.pron);
-//        conjunctionsFound2 = JMDictEntryRepo2.collectWordsOrPronOfEntries(conjunctionsByPron, CollectionMode.pron);
-//        nounsFound2 = JMDictEntryRepo2.collectWordsOrPronOfEntries(nounsByPron, CollectionMode.pron)
-//        prefixesFound2 = JMDictEntryRepo2.collectWordsOrPronOfEntries(prefixesByPron, CollectionMode.pron);
-//        adjectivesFound2 = JMDictEntryRepo2.collectWordsOrPronOfEntries(adjectivesByPron, CollectionMode.pron);
-//        adnominalsFound2 = JMDictEntryRepo2.collectWordsOrPronOfEntries(adnominalsByPron, CollectionMode.pron);
-//        exclamationsFound2 = JMDictEntryRepo2.collectWordsOrPronOfEntries(exclamationsByPron, CollectionMode.pron);
-//        symbolsFound2 = JMDictEntryRepo2.collectWordsOrPronOfEntries(symbolsByPron, CollectionMode.pron);
-//        fillersFound2 = JMDictEntryRepo2.collectWordsOrPronOfEntries(fillersByPron, CollectionMode.pron);
-//        othersFound2 = JMDictEntryRepo2.collectWordsOrPronOfEntries(othersByPron, CollectionMode.pron);
-//        unclassifiedFound2 = JMDictEntryRepo2.collectWordsOrPronOfEntries(unclassifiedByPron, CollectionMode.pron);
-//        ;
 
         TokensByMecabPOS.updateTokensRemainingToBeSearched(tokensByMecabPOS, pronsFoundByMecabPOSKatakana, tokensToSearch, Mode.READINGS_IN_KATAKANA);
-        // TODO: figure out whether verb's token.getReading() needs convertKana (probably does), or at least remove it altogether.
-//        particles.forEach(token -> { if(particlesFound2.contains(token.getReading())) tokensToSearch.remove(token); });
-//        verbsAndAux.forEach(token -> { if(verbsFound2.contains(token.getBaseForm())) tokensToSearch.remove(token); });
-//        adverbs.forEach(token -> { if(adverbsFound2.contains(token.getReading())) tokensToSearch.remove(token); });
-//        conjunctions.forEach(token -> { if(conjunctionsFound2.contains(token.getReading())) tokensToSearch.remove(token); });
-//        nouns.forEach(token -> { if(nounsFound2.contains(token.getReading())) tokensToSearch.remove(token); });
-//        prefixes.forEach(token -> { if(prefixesFound2.contains(token.getReading())) tokensToSearch.remove(token); });
-//        adjectives.forEach(token -> { if(adjectivesFound2.contains(token.getReading())) tokensToSearch.remove(token); });
-//        adnominals.forEach(token -> { if(adnominalsFound2.contains(token.getReading())) tokensToSearch.remove(token); });
-//        exclamations.forEach(token -> { if(exclamationsFound2.contains(token.getReading())) tokensToSearch.remove(token); });
-//        symbols.forEach(token -> { if(symbolsFound2.contains(token.getReading())) tokensToSearch.remove(token); });
-//        fillers.forEach(token -> { if(fillersFound2.contains(token.getReading())) tokensToSearch.remove(token); });
-//        others.forEach(token -> { if(othersFound2.contains(token.getReading())) tokensToSearch.remove(token); });
-//        unclassified.forEach(token -> { if(unclassifiedFound2.contains(token.getReading())) tokensToSearch.remove(token); });
         // tokensToSearch == 65. All seem to be proper nouns, or conjugative particles like -ta.
 
 //        cumulative.forEach(row -> {
