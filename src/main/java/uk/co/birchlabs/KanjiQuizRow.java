a2b0cf49ee1eece9dd99a8a1e557513611e30fc3
Jamie Birch <jbizzle5@gmail.com>
2016-07-13 15:16:33 +0100

    kanji quiz done

--- src/main/java/uk/co/birchlabs/KanjiQuizRow.java
+++ src/main/java/uk/co/birchlabs/KanjiQuizRow.java
@@ -10,24 +10,33 @@ import static uk.co.birchlabs.EntryReadout.PRONS_START_KEY;
  * Created by jamiebirch on 13/07/2016.
  */
 public class KanjiQuizRow implements QuizRow {
-    private final String info; // becomes pron + definition
-    private final String quiz;
+    private final String info; // 使徒
+    private final String quizTarget; // [しと]：disciple ･ apostle
 
+    /**
+     * Expects input like: 使徒 [しと]：disciple ･ apostle
+     *        ... or like: スタッフ：(1) staff; (2) stuff
+     * @param bf
+     * @param fullDef
+     */
     public KanjiQuizRow(String bf, String fullDef) {
-        String kanjiAndMeanings = fullDef.split(Pattern.quote(MEANINGS_START_KEY), 2)[0];
+        this.info = bf;
 
-        if(kanjiAndMeanings.contains(PRONS_START_KEY) && kanjiAndMeanings.contains(PRONS_END_KEY))
-            this.info = fullDef.split(Pattern.quote(PRONS_START_KEY), 2)[1];
-        this.quiz = bf;
+        String kanjiAndMeanings = fullDef.split(Pattern.quote(MEANINGS_START_KEY), 2)[0]; // 使徒 [しと]
+        if(kanjiAndMeanings.contains(PRONS_START_KEY) && kanjiAndMeanings.contains(PRONS_END_KEY)) {
+            this.quizTarget = PRONS_START_KEY + fullDef.split(Pattern.quote(PRONS_START_KEY), 2)[1].split(Pattern.quote(PRONS_END_KEY))[0] + PRONS_END_KEY
+                    + MEANINGS_START_KEY + fullDef.split(Pattern.quote(MEANINGS_START_KEY), 2)[1];
+        }
+        else throw new IllegalStateException("This definition has no kanji form to test upon; it is purely phonetic.");
     }
 
     @Override
     public String getInfoPortion() {
-        return null;
+        return info;
     }
 
     @Override
     public String getQuizPortion() {
-        return null;
+        return quizTarget;
     }
 }
