58f526402edf1446d37e607891574ff09633f28e
Jamie Birch <jbizzle5@gmail.com>
2016-07-28 13:38:17 +0100

    authorisation for Spring updated

--- src/main/java/uk/co/birchlabs/WebSecurityConfig.java
+++ src/main/java/uk/co/birchlabs/WebSecurityConfig.java
@@ -43,6 +43,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER");
+                .withUser("topicdictSpring").password("deadlineapproachingSpring").roles("USER");
     }
 }
